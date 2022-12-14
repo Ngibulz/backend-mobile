61e86e61510e0288fdf7f73b30c4898b3aeb9e03
Ngibulz <william.putra@student.umn.ac.id>
2022-12-14 21:48:52 +0700

    enable migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -33,9 +33,9 @@ import { AliOssHelperModule } from './ali-oss/ali-oss.module';
           __dirname + '/migrations/*{.ts,.js}'
          ],
         autoLoadEntities:true,
-        migrationsRun:false,
+        migrationsRun:true,
         migrationsTableName: "migrations_typeorm",
-        synchronize: true,
+        synchronize: false,
       })
     }),
     UserModule,
