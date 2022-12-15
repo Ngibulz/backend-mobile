4e9e2ecf6641e86cbd832f5ed4e66f964bd098d2
Ngibulz <william.putra@student.umn.ac.id>
2022-12-15 23:16:43 +0700

    increase limit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { UnauthorizedException } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,8 @@ async function bootstrap() {
     "preflightContinue": false,
     "optionsSuccessStatus": 204
   })
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   app.useGlobalFilters(new UnauthorizedExceptionFilter);
   app.useGlobalFilters(new ForbiddenExceptionFilter);
   await app.listen(process.env.PORT || 80)}
